af6f1f47987706bd0ef9bc64b5556af533cc9856
Vinicius Frias <vmofrias@gmail.com>
2025-03-25 20:32:54 -0300

    chore: grpc microservices created

---
+++ client-grpc-microservice/src/main/java/com/ldsk/ClientGrpcMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ldsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGrpcMicroserviceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClientGrpcMicroserviceApplication.class, args);
+    }
+}
